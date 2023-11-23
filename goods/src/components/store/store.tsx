868f122c895690179fa0c1d041193d11916b704f
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2023-11-23 10:46:04 +0300

    upd

--- goods/src/components/store/store.tsx
+++ goods/src/components/store/store.tsx
@@ -3,10 +3,22 @@ import { products} from '../db/store/store'
 export function CardRender ({ style }: {style:string}) {
 
     return (
-        <div className={'card-container ' + style}>
+        <div 
+            className={'card-container'}
+            style={style==='view_list' ? {display:'table-row'} : {display: 'flex'}}
+            >
         {products.map((product, index) => (
-            <div className={'ShopCard ' + style} key={index}>
-            <img src={product.img} alt={product.name} />
+            <div className={'ShopCard '} key={index} style={style==='view_list' ? {
+                display:'-webkit-box',
+                width: '100%',
+                height: '100px'
+                } : {display: 'block'}}>
+            <img src={product.img} alt={product.name} style={style==='view_list' ? {
+                width:'100%',
+                height: '100%'} : {
+                height: '50%',
+                width:'50%'
+                }} />
             <h3>{product.name}</h3>
             <p>{product.price}</p>
             <p>{product.color}</p>
