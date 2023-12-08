4b81a79bea62c4c7df125f7350557c3475733699
keplerxiii <a.v.pushkin@gmail.com>
2023-12-08 11:30:24 +0300

    upd

--- goods/src/App.tsx
+++ goods/src/App.tsx
@@ -5,16 +5,16 @@ import { ProductCardList } from './components/store/store'
 function App() {
   const [state, setState] = useState('view_module')
 
-  function clickHandler () {
-    if (state === 'view_list') {setState('view_module')} 
-    else {setState('view_list')}
-    }
+  function clickHandler() {
+    if (state === 'view_list') { setState('view_module') }
+    else { setState('view_list') }
+  }
 
 
   return (
     <div>
       <button className="material-icons" onClick={clickHandler}> {state} </button>
-      <ProductCardList style={state}/>
+      <ProductCardList style={state} />
     </div>
   )
 }
--- goods/src/components/store/store.tsx
+++ goods/src/components/store/store.tsx
@@ -1,14 +1,20 @@
-import { products } from '../db/store/store';
+import { products } from "../db/store/store";
 
 export function ProductCardList({ style }: { style: string }) {
   const cardContainerStyle =
-    style === 'view_list' ? { display: 'table-row' } : { display: 'flex' };
-
+    style === "view_list" ? { display: "table-row" } : { display: "flex" };
   return (
-    <div className={'card-container'} style={cardContainerStyle}>
+    <div className={"card-container"} style={cardContainerStyle}>
       {products.map((product, index) => (
-        <div className={'ShopCard'} key={index} style={getStyleForCard(style)}>
-          <img src={product.img} alt={product.name} style={getImageStyle(style)} />
+        <div
+          className={"ShopCard"}
+          key={index}
+          style={getStyleForCard(style)}>
+          <img
+            src={product.img}
+            alt={product.name}
+            style={getImageStyle(style)}
+          />
           <h3>{product.name}</h3>
           <p>{product.price}</p>
           <p>{product.color}</p>
@@ -16,29 +22,30 @@ export function ProductCardList({ style }: { style: string }) {
         </div>
       ))}
     </div>
-  );
+  )
 }
 
 function getStyleForCard(style: string) {
-  return style === 'view_list'
+  return style === "view_list"
     ? {
-      display: '-webkit-box',
-      width: '100%',
-      height: '100px',
+      display: "-webkit-box",
+      width: "100%",
+      height: "100px",
+
     }
     : {
-      display: 'block',
+      display: "block",
     };
 }
 
 function getImageStyle(style: string) {
-  return style === 'view_list'
+  return style === "view_list"
     ? {
-      width: '100%',
-      height: '100%',
+      width: "100%",
+      height: "100%",
     }
     : {
-      height: '50%',
-      width: '50%',
-    };
+      height: "50%",
+      width: "50%",
+    }
 }
